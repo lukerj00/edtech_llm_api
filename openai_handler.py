af1cddda00bdc65ae1533ca2b1a50522c30dce42
lukerj00 <luke.johnston00@gmail.com>
2024-09-13 11:20:02 +0100

    edit response format to be in line with prev version

--- openai_handler.py
+++ openai_handler.py
@@ -38,7 +38,7 @@ class OpenAIHandler:
                 "assistant_id": assistant_id,
                 "thread_id": thread_id,
                 "submission": submission,
-                "feedback": [formatted_output]
+                "feedback": formatted_output
                 }
         
         except openai.OpenAIError as e:
